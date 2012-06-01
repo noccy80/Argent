570f6c0d51f7ccc0a2881131750b896ea158958d
Ryan J. Jung <gradysghost@gmail.com>
2012-05-31 19:06:58 -0500

    Issue #20 fixed. Dependency on JQuery removed!

--- docroot/js/session_handlers.js
+++ docroot/js/session_handlers.js
@@ -2,62 +2,46 @@
  * btnStart_click()
  * btnSet_click()
  * btnGet_click()
- * btnSave_click()
- * btnSnapshot_click()
  */
 
 function btnStart_click() {
-	$.ajax({
-		url:ajaxUrl,
-		type:"GET",
-		dataType:"json",
-		data:{
-			"action":"session@renew"
-		},
-		success:function(data, status, xhr) {
-			$("#pSid").text("Session ID: " + data.sid);
-		},
-		error:function(xhr, status, error) {
-			alert(error + "\n" + status + "\n" + JSON.stringify(xhr));
+	// Fuck IE.
+	var xhrStart = new XMLHttpRequest();
+	xhrStart.open('GET', ajaxUrl + "?action=session@renew", true);
+	xhrStart.onerror = function(e){
+		alert("Could not start a session.");
+	};
+	xhrStart.onreadystatechange = function() {
+		if (xhrStart.readyState == 4) {
+			document.getElementById("pSid").innerHTML = "Session ID: " + JSON.parse(xhrStart.responseText).sid;
 		}
-	});
+	};
+	xhrStart.send(null);
 }
 
 function btnSet_click() {
-	$.ajax({
-		url:ajaxUrl,
-		type:"GET",
-		dataType:"json",
-		data:{
-			"action":"session@set",
-			"key":$("#txtKey").val(),
-			"value":$("#txtData").val()
-		},
-		success:function(data, status, xhr) {
-			// Do nothing
-		},
-		error:function(xhr, status, error) {
-			alert(error);
-		}
-	});
+	var xhrSet = new XMLHttpRequest();
+	var key = document.getElementById("txtKey").value;
+	var val = document.getElementById("txtData").value;
+	xhrSet.open('GET', ajaxUrl + "?action=session@set&key=" + key + "&value=" + val, true);
+	xhrSet.onerror = function(e) {
+		alert("Could not set key.");
+	};
+	xhrSet.onreadystatechange = function(){};
+	xhrSet.send(null);
 }
 
 function btnGet_click() {
-	$.ajax({
-		url:ajaxUrl,
-		type:"GET",
-		dataType:"json",
-		data:{
-			"action":"session@get",
-			"key":$("#txtKeyGet").val()
-		},
-		success:function(data, status, xhr) {
-			$("#pGet").text(data.value);
-		},
-		error:function(xhr, status, error) {
-			alert(error);
-		}
-	});
+	var xhrGet = new XMLHttpRequest();
+	var key = document.getElementById("txtKeyGet").value;
+	xhrGet.open('GET', ajaxUrl + "?action=session@get&key=" + key, true);
+	xhrGet.onerror = function(e) {
+		alert("Could not get key.");
+	};
+	xhrGet.onreadystatechange = function(){
+		document.getElementById("pGet").innerHTML = "Value: " + JSON.parse(xhrGet.responseText).value;
+	};
+	xhrGet.send(null);
 }
 
 var ajaxUrl = "http://%SERVER_URL%:%LISTEN_PORT%";
